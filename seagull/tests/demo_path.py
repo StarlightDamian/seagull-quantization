a59b7cf5e61b50e7ddd6374498cc213361ea04e9
Damian <zengyuwei1995@163.com>
2025-06-17 11:05:54 +0800

    setup

---
+++ seagull/tests/demo_path.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@Date       : 2025/6/17 10:52
+@Author     : Damian
+@Email      : zengyuwei1995@163.com
+@File       : demo_path.py
+@Description: 
+"""
+from seagull import PATH
+print(PATH)
---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Date       : 2025/6/17 11:04
+@Author     : Damian
+@Email      : zengyuwei1995@163.com
+@File       : setup.py
+@Description: 
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="seagull",
+    version="0.0.1",
+    packages=find_packages(),
+)
